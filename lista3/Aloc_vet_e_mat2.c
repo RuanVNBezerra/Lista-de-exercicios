385de16017f562d1070b90e0c56867bc82078166
RuanVNBezerra <164013416+RuanVNBezerra@users.noreply.github.com>
2024-07-31 16:09:29 -0300

    alteração

--- lista3/Aloc_vet_e_mat2.c
+++ lista3/Aloc_vet_e_mat2.c
@@ -5,25 +5,24 @@ int main() {
     int linhas, colunas, i, j;
     int **matriz;
 
-    // Lendo o número de linhas e colunas
+    
     printf("Digite o número de linhas da matriz: ");
     scanf("%d", &linhas);
     printf("Digite o número de colunas da matriz: ");
     scanf("%d", &colunas);
 
-    // Alocando memória para a matriz
     matriz = (int **)malloc(linhas * sizeof(int *));
     for (i = 0; i < linhas; i++) {
         matriz[i] = (int *)malloc(colunas * sizeof(int));
     }
 
-    // Verificando se a alocação foi bem-sucedida
+    
     if (matriz == NULL) {
         printf("Erro ao alocar memória!\n");
         return 1;
     }
 
-    // Lendo os elementos da matriz
+    
     printf("Digite os elementos da matriz:\n");
     for (i = 0; i < linhas; i++) {
         for (j = 0; j < colunas; j++) {
@@ -31,7 +30,6 @@ int main() {
         }
     }
 
-    // Imprimindo a matriz original
     printf("\nMatriz original:\n");
     for (i = 0; i < linhas; i++) {
         for (j = 0; j < colunas; j++) {
@@ -40,7 +38,6 @@ int main() {
         printf("\n");
     }
 
-    // Imprimindo a matriz transposta
     printf("\nMatriz transposta:\n");
     for (i = 0; i < colunas; i++) {
         for (j = 0; j < linhas; j++) {
@@ -49,7 +46,6 @@ int main() {
         printf("\n");
     }
 
-    // Liberando a memória alocada
     for (i = 0; i < linhas; i++) {
         free(matriz[i]);
     }
--- lista7/struct_union_enum3.c
+++ lista7/struct_union_enum3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Definicao do enum para os meses do ano
+
 typedef enum {
     JANEIRO,
     FEVEREIRO,
@@ -16,28 +16,28 @@ typedef enum {
     DEZEMBRO
 } Mes;
 
-// Definicao da struct Data
+
 typedef struct {
     int dia;
     Mes mes;
     int ano;
 } Data;
 
-// Funcao para ler uma data
+
 void lerData(Data *d) {
     int mes;
     printf("Digite o dia: ");
     scanf("%d", &d->dia);
     printf("Digite o mes (1-12): ");
     scanf("%d", &mes);
-    d->mes = mes - 1; // Ajusta o valor para o enum (0-11)
+    d->mes = mes - 1; 
     printf("Digite o ano: ");
     scanf("%d", &d->ano);
 }
 
-// Funcao para imprimir uma data no formato dd/mm/aaaa
+
 void imprimirData(Data d) {
-    printf("%02d/%02d/%04d\n", d.dia, d.mes + 1, d.ano); // Ajusta o valor do mes para a impressao (1-12)
+    printf("%02d/%02d/%04d\n", d.dia, d.mes + 1, d.ano);
 }
 
 int main() {
